785ce7546a884fec2d1d5e61673550039e687e21
王迎国 <127494066+wangyingguo@users.noreply.github.com>
2023-06-21 16:08:40 +0800

    Update MyApplicaiton.kt

--- app/src/main/java/com/example/androiddemo/utils/MyApplicaiton.kt
+++ app/src/main/java/com/example/androiddemo/utils/MyApplicaiton.kt
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
 
+TODO：讲名字改为MainApplication，放在app根目录下。
+这是程序的入口，放在utils下算怎么回事
+
 class MyApplicaiton:Application() {
     companion object{
         @SuppressLint("StaticFieldLeak")
